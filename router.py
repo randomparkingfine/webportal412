689e4caf70d0195702447d4a1332fa3793ee1311
shockrahwow <alejandros714@protonmail.com>
2019-12-03 12:35:36 -0800

    secret key for sessions ready

--- router.py
+++ router.py
@@ -1,20 +1,22 @@
+import secrets
 from flask import Flask, session
 from flask_session import Session
 from flask import request
 from flask import render_template
-
-sesh = Session()
+from random import randint
 
 app = Flask(__name__)
-sesh.init_app(app)
+app.config['SECRET_KEY'] = secrets.token_urlsafe(32)
 
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
     if request.method == 'GET':
         return render_template('index.html')
+
     elif request.method == 'POST':
         pass
+
     else:
         return 'Method not supported', 400
 
