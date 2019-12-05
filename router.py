7e4ce3aca7d4b20c0bc8d69e9d7bab9880b28d4a
Michael <mavalos-garcia@csumb.edu>
2019-12-04 18:01:31 -0800

    gang

--- router.py
+++ router.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 import mysql.connector
 import secrets
 from flask import Flask, session
@@ -6,12 +8,12 @@ from flask import request, redirect, url_for
 from flask import render_template
 from random import randint
 
-
 query = "INSERT INTO  assets(asset_no, m_name, m_address, m_phone, m_website, model, purchase_date, price, exp_date, retire_date, description, comments) " \
-            "VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+        "VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = secrets.token_urlsafe(32)
+
 
 class DB:
     def __init__(self):
@@ -22,93 +24,99 @@ class DB:
         # dbname: czb69utqx3tir481
 
         self.config = {
-            'user':'hai32064fkjrmfsl',
-            'password':'k0jmbqf380zmenkj',
-            'host':'bfjrxdpxrza9qllq.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-            'port':'3306',
-            'database':'czb69utqx3tir481',
+            'user': 'hai32064fkjrmfsl',
+            'password': 'k0jmbqf380zmenkj',
+            'host': 'bfjrxdpxrza9qllq.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+            'port': '3306',
+            'database': 'czb69utqx3tir481',
         }
         self.conn = mysql.connector.connect(**self.config)
+
+
+    def search(self, **values):
+        pass
+
+    def insert_asset(self, values):
+        # generate the format string we need to actually build the meme
+        now = datetime.now().date()
+        ins_str = 'INSERT INTO assets(asset_no, m_name, m_address,' \
+                  'm_phone, m_website, model, purchase_date,' \
+                  'price, exp_date, retire_date, description, comments)' \
+                  'VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)'
+        cursor = self.conn.cursor()
+        cursor.execute(ins_str, values)
+
+    def delete(self, key):
+        d_str = 'DELETE FROM assets WHERE asset_no=?'
+        cursor = self.conn.cursor()
+        cursor.execute(d_str, (key))
+        self.conn.commit()
+
+    def close(self):
         curs = self.conn.cursor(buffered=True)
         curs.close()
         self.conn.close()
-        #cursor.execute("CREATE TABLE assets (name TEXT)")
-        
-
-    def insert_asset(self, args):
-    	self.conn = mysql.connector.connect(**self.config)
-    	curs = self.conn.cursor(buffered=True)
-    	curs.execute(query, args)
-    	print(curs.execute("SHOW TABLES"))
-    	curs.close()
-    	self.conn.close()
-
-    def select_query(self):
-    	sql_select_query = "select * from assets"
-    	self.conn = mysql.connector.connect(**self.config)
-    	curs = self.conn.cursor(buffered=True)
-    	curs.execute(sql_select_query)
-    	records = curs.fetchall()
-    	print("Total number of rows in assets is: ", curs.rowcount)
-    	self.conn.close()
 
 
-        
 @app.route('/', methods=['GET', 'POST'])
 def home():
     if request.method == 'GET':
         return render_template('index.html')
 
     elif request.method == 'POST':
-    	if request.form['submit'] == 'search':
-    		session['txtSearchAsset'] = request.form['txtSearchAsset']
-    		return redirect(url_for('asset'))
-    	else:
-    		database = DB()
-    		assest_num = request.form['txtAssetNumber']
-    		nm = request.form['txtManufacturerName']
-    		addr = request.form['txtManufacturerAddress']
-    		phone = request.form['txtManufacturerPhone']
-    		web = request.form['txtManufacturerWebPage']
-    		model = request.form['txtManufacturer']
-    		date_purch = request.form['txtDatePurchased']
-    		price = request.form['txtPurchasePrice']
-    		expr = request.form['txtExpirationDate']
-    		retired = request.form['txtRetiredDate']
-    		descript = request.form['txtDescription']
-    		comments = request.form['txtComments']
-    		#args = (assest_num, nm, addr, phone, web, model, date_purch, price, expr, retired, descript, comments)
-    		#database.insert_asset(args)
+        if request.form['submit'] == 'search':
+            session['txtSearchAsset'] = request.form['txtSearchAsset']
+            return redirect(url_for('asset'))
+        else:
+            database = DB()
+            assest_num = request.form['txtAssetNumber']
+            nm = request.form['txtManufacturerName']
+            addr = request.form['txtManufacturerAddress']
+            phone = request.form['txtManufacturerPhone']
+            web = request.form['txtManufacturerWebPage']
+            model = request.form['txtManufacturer']
+            date_purch = request.form['txtDatePurchased']
+            price = request.form['txtPurchasePrice']
+            expr = request.form['txtExpirationDate']
+            retired = request.form['txtRetiredDate']
+            descript = request.form['txtDescription']
+            comments = request.form['txtComments']
+            args = (assest_num, nm, addr, phone, web, model, date_purch, price, expr, retired, descript, comments)
+            database.insert_asset(args)
 
     else:
         return 'Method not supported', 400
     return render_template('index.html')
 
+
 @app.route('/asset', methods=['GET', 'POST'])
 def asset():
-	info = {
-		'manufacturer':'Asus',
-		'asset_num': '1321',
-		'manufacturer_Addr': '',
-		'manu_Phone': '',
-		'manuWeb': '',
-		'model': '',
-		'purchased_Date': '',
-		'purchase_Price': '',
-		'warrantyExpDate': '',
-		'retired_Date': '',
-		'description': '',
-		'comments': '',
-	}
-	if request.method == 'POST':
-		
-		if request.form['submit'] == 'Update':
-			return redirect(url_for('home'))
+    database = DB()
 
-		elif request.form['submit'] == 'Return':
-			return redirect(url_for('home'))
+    info = {
+        'manufacturer': 'asdjfkl',
+        'asset_num': 'shoothebox',
+        'manufacturer_Addr': 'ads',
+        'manu_Phone': 'adf',
+        'manuWeb': 'adfs',
+        'model': 'adf',
+        'purchased_Date': 'dd',
+        'purchase_Price': 'sss',
+        'warrantyExpDate': 'dd',
+        'retired_Date': 'ss',
+        'description': 'ss',
+        'comments': 'dd',
+    }
+    if request.method == 'POST':
 
-	return render_template('assets.html', data=info)
+        if request.form['submit'] == 'Update':
+            return redirect(url_for('home'))
+
+        elif request.form['submit'] == 'Return':
+            return redirect(url_for('home'))
+
+    return render_template('assets.html', data=info)
+
 
 """@app.route('/search', methods=['POST'])
 def search():
